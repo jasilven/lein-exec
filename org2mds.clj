bbc5590625ebbfcdccd8229476cf4e7a2705d1bc
jasilven <jasilven@gmail.com>
2019-07-13 22:28:56 +0300

    simplified parsing

--- org2mds.clj
+++ org2mds.clj
@@ -8,44 +8,26 @@ org-file's top level headings should be formatted like this:
 usage:
 % lein-exec org2mds.clj <org-file> "}
  org2mds
-  (:require [clojure.string :as str])
+  (:require [clojure.string :as s])
   (:require [clojure.java.io :as io])
   (:import java.text.SimpleDateFormat))
 
-(def re-title (re-pattern "^\\* (.*)<(\\d{4}?-\\d\\d-\\d\\d ... \\d\\d:\\d\\d)>\\s*$"))
+(def re-title (re-pattern "^(.+)<(.+)>"))
 (def datefmt (SimpleDateFormat. "yyyy-MM-dd EEE kk:mm"))
 
-(defn add-note
-  "add note to notes and return update notes"
-  [notes line content]
-  (let [[_ title date] (re-matches re-title line)]
-    (conj notes {:title title
-                 :date date
-                 :content content})))
-
 (defn parse-notes
-  "parse org notes and return them as vector"
-  ([fname]
-   (parse-notes nil [] [] (str/split-lines (slurp fname))))
-  ([title content notes [line & lines]]
-   (cond
-     (nil? line)
-     (if (nil? title)
-       notes
-       (add-note notes title content))
-
-     (re-matches re-title line)
-     (if (nil? title)
-       (recur line content notes lines)
-       (recur line [] (add-note notes title content) lines))
-
-     :else
-     (recur title (conj content line) notes lines))))
+  "parse org notes and return them as seq"
+  [fname]
+  (let [data (clojure.core/str "\n" (s/triml (slurp fname)))
+        notes (s/split data #"\n\* ")]
+    (for [note (rest notes)
+          :let [[_ title date] (re-find re-title note)]]
+      {:title (s/trim title) :date date :content (s/replace-first note #"^.*\n?" "")})))
 
 (defn unique-fname
   "return unique filename for name by adding number to filename if needed: 'name-<number>.ext'"
   [name ext]
-  (let [name (str/replace name "/" " ")]
+  (let [name (s/replace name "/" " ")]
     (->>
      (conj (for [n (rest (range))] (str name "-" n "." ext))
            (str name "." ext))
@@ -57,7 +39,7 @@ usage:
   [note]
   (let [time (.getTime (.parse datefmt (:date note)))
         fname (unique-fname (:title note) "md")]
-    (spit fname (apply str (map #(str % "\n") (:content note))))
+    (spit fname (str "# " (:title note) "\n" (:date note) "\n\n" (:content note)))
     (.setLastModified (io/file fname) time)))
 
 (defn run [fname]
